19d9fe8c2ccd92d90f767f9c7235aa32fcb474c3
NikhilBelide <66310861+BelideNikhil@users.noreply.github.com>
2021-06-01 00:26:52 +0530

    add working skeleton of multi translator

--- app.js
+++ app.js
@@ -2,12 +2,36 @@ console.log("hello")
 var input_text=document.querySelector("#textarea");
 var translate_btn=document.querySelector("#translator");
 var output_catch=document.querySelector("#output");
+var chooser=document.querySelector("#choose")
+var keys="";
 
-var serverURL = "https://api.funtranslations.com/translate/minion.json";
+// ===========================storage========================
+var storageURL=[{k:"minion",u:"https://api.funtranslations.com/translate/minion.json"},
+{k:"yoda",u:"https://api.funtranslations.com/translate/yoda.json"},
+{k:"morse",u:"https://api.funtranslations.com/translate/morse.json"}
+]
 
+var serverURL;
+// =========================translator chooser=================
+chooser.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        keys=chooser.value;
+        for(var i=0;i<storageURL.length;i++){
+            var holder=storageURL[i].k;
+            if(keys===holder){
+                serverURL=storageURL[i].u
+                console.log("pass")
+                console.log( serverURL)
+            }
+        }
+    }
+});
+
+// ======================fina url creator==========================
 function createURL(){
-    return serverURL +"?"+"text=" +input_text.value
+   return serverURL +"?"+"text=" +input_text.value
 }
+
 function errorHandler(error) {
     console.log("error occured", error);
     alert("something wrong with server! try again after some time")
@@ -16,7 +40,7 @@ translate_btn.addEventListener("click", ()=>{
     
     translate_btn.innerHTML="Your data is being translated..."
     setTimeout(()=>{
-        translate_btn.innerHTML="Translate"},3000);
+        translate_btn.innerHTML="Translate"},1500);
         fetch(createURL(input_text.value))
        .then(response =>response.json())
        .then(json=>{
