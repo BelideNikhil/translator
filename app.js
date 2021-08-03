cc4880572f374173dfbba505c2117956dd17b983
NikhilBelide <66310861+BelideNikhil@users.noreply.github.com>
2021-08-04 03:48:58 +0530

    refactor color

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 console.log("hello")
-var translate_btn=document.querySelector("#translator");
-var input_text=document.querySelector("#textarea");
-var output_catch=document.querySelector("#output");
+const translate_btn=document.querySelector("#translator");
+let input_text=document.querySelector("#textarea");
+let output=document.querySelector("#output");
 
-var serverURL = "https://api.funtranslations.com/translate/minion.json";
+const serverURL = "https://api.funtranslations.com/translate/minion.json";
 
 function createURL(){
     return serverURL +"?"+"text=" +input_text.value
 }
 function errorHandler(error) {
     console.log("error occured", error);
-    output_catch.innerText="Something wrong with server! try again after some time";
+    output.innerText="Something is wrong with server! Please try after some time.";
     setTimeout(()=>{
         input_text.value='';
-        output_catch.innerText='';
-    },2000)
+        output.innerText='';
+    },2500)
     
 }
 translate_btn.addEventListener("click", ()=>{
@@ -22,14 +22,15 @@ translate_btn.addEventListener("click", ()=>{
         fetch(createURL(input_text.value))
         .then(response =>response.json())
         .then(json=>{
-            output_catch.innerText = json.contents.translated;
+            console.log(json.contents)
+            output.innerHTML = json.contents.translated;
         })
         .catch(errorHandler)
 
     }else{
-        output_catch.innerText = "Please enter some data to process it";
+        output.innerText = "Please enter some data to process it";
         setTimeout(()=>{
-            output_catch.innerText=''
+            output.innerText=''
         },2500)
     }
         
